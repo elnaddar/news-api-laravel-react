4745a4b4e9b50a8b2c7791f3853689f0ffb69a6e
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-11-30 12:59:03 +0200

    feat(app): create NewsIndex.

--- frontend/src/components/news/NewsIndex.jsx
+++ frontend/src/components/news/NewsIndex.jsx
@@ -1,5 +1,14 @@
+import NewsIndexViewer from "./NewsIndexViewer";
+
 function NewsIndex() {
-  return <div>NewsIndex</div>;
+  return (
+    <div className="container mt-4">
+      <h1 className="display-1 text-center mb-5">News Time</h1>
+      <div className="row">
+        <NewsIndexViewer />
+      </div>
+    </div>
+  );
 }
 
 export default NewsIndex;
