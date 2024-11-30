bbbba2e08cc724c76b17ccac6e1b87a95b552f74
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-11-30 08:02:31 +0200

    feat(api): create NewsFactory with fancy headlines and images.

--- news-api/database/factories/NewsFactory.php
+++ news-api/database/factories/NewsFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +10,9 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class NewsFactory extends Factory
 {
+    const FAKER_IMAGES_PATH = "images";
+    const IMAGES_STORAGE_PATH = "app/public/" . NewsFactory::FAKER_IMAGES_PATH;
+
     /**
      * Define the model's default state.
      *
@@ -16,8 +20,22 @@ class NewsFactory extends Factory
      */
     public function definition(): array
     {
+        // Create images directory if not exists
+        Storage::disk("public")->makeDirectory($this::FAKER_IMAGES_PATH);
+
+        // Download fake image and store it
+        $fakerImage = $this->faker->image(
+            storage_path($this::IMAGES_STORAGE_PATH),
+            800,
+            600
+        );
+
         return [
-            //
+            "title" => $this->faker->headline(),
+            "source" => $this->faker->NewssourceName(),
+            "author" => $this->faker->name(),
+            "content" => $this->faker->realText(1000),
+            "image" => $this::IMAGES_STORAGE_PATH . basename($fakerImage)
         ];
     }
 }
