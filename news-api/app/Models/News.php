8d6b91415ede031be2f309d76d86baf1f197af4d
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-11-30 08:43:24 +0200

    feat(api): add fillable fields to News model.

--- news-api/app/Models/News.php
+++ news-api/app/Models/News.php
@@ -9,4 +9,6 @@ class News extends Model
 {
     /** @use HasFactory<\Database\Factories\NewsFactory> */
     use HasFactory;
+
+    protected $fillable = ["title", "content", "source", "image", "author"];
 }
