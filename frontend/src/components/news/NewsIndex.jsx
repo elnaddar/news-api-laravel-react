95745196034a7b5c243223af65aa1aa6ef017b59
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-11-30 13:31:54 +0200

    feat(app): used search params to paginate content.

--- frontend/src/components/news/NewsIndex.jsx
+++ frontend/src/components/news/NewsIndex.jsx
@@ -1,11 +1,13 @@
 import NewsIndexViewer from "./NewsIndexViewer";
+import { useSearchParams } from "react-router-dom";
 
 function NewsIndex() {
+  const [searchParams] = useSearchParams();
   return (
     <div className="container mt-4">
       <h1 className="display-1 text-center mb-5">News Time</h1>
       <div className="row">
-        <NewsIndexViewer />
+        <NewsIndexViewer params={{ page: searchParams.get("page") }} />
       </div>
     </div>
   );
--- frontend/src/components/news/NewsIndexViewer.jsx
+++ frontend/src/components/news/NewsIndexViewer.jsx
@@ -2,14 +2,17 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import NewsCard from "./NewsCard";
 
-function NewsIndexViewer() {
+function NewsIndexViewer(props) {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const { params } = props;
+  
   useEffect(() => {
     setLoading(true);
     const fetchNews = async () => {
-      const response = await axios.get("http://127.0.0.1:8000/api/news");
+      const response = await axios.get("http://127.0.0.1:8000/api/news", {
+        params: params,
+      });
       setArticles(response.data.data);
       setLoading(false);
     };
