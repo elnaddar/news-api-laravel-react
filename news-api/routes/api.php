675be123c07e97ec3dd56cc28b1b95274f9cab4b
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-11-30 09:13:43 +0200

    feat(api): create api resource for NewsController.

--- news-api/routes/api.php
+++ news-api/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\NewsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource("news", NewsController::class);
+
+//
