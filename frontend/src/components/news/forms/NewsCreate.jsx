31faf253de3723f2bbfa46496d0f8282caa13c40
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-12-01 06:39:47 +0200

    feat(app): finish form create request

--- frontend/src/components/news/forms/NewsCreate.jsx
+++ frontend/src/components/news/forms/NewsCreate.jsx
@@ -1,11 +1,22 @@
+import { NEWS_API } from "../../../shared/constants";
 import NewsForm from "./NewsForm";
+import axios from "axios";
 
 function NewsCreate() {
+  const handleFormSubmit = async function (event) {
+    return await axios.post(NEWS_API, event.target, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
   return (
     <div className="container mt-4">
       <h1 className="display-1 text-center mb-5">Add Article</h1>
-
-      <NewsForm />
+      <NewsForm
+        successMessage="Created successfully"
+        onFormSubmit={handleFormSubmit}
+      />
     </div>
   );
 }
--- frontend/src/components/news/forms/NewsForm.jsx
+++ frontend/src/components/news/forms/NewsForm.jsx
@@ -1,81 +1,121 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function NewsForm(props) {
-  const defaults = props.defaults ?? {
-    title: "",
-    author: "",
-    source: "",
-    content: "",
+  const defaults = props.defaults ?? {};
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [responseData, setResponseData] = useState({});
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsSubmitting(true);
+    const response = await props.onFormSubmit(event);
+    setResponseData(response.data);
+    event.target.reset();
+    setIsSubmitting(false);
+  };
+
+  const handleAlertClose = () => {
+    setResponseData({});
   };
   return (
-    <form action="">
-      <div className="mb-3">
-        <label htmlFor="title" className="form-label">
-          Title
-        </label>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          className="form-control"
-          placeholder="Title"
-          aria-describedby="helpId"
-          defaultValue={defaults.title}
-        />
-        <small id="helpId" className="text-muted">
-          This is my First article and I say Hello.
-        </small>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="author" className="form-label">
-          Author
-        </label>
-        <input
-          type="text"
-          name="author"
-          id="author"
-          className="form-control"
-          placeholder="Author"
-          aria-describedby="helpId"
-          defaultValue={defaults.author}
-        />
-        <small id="helpId" className="text-muted">
-          Ahmed Mahdy.
-        </small>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="source" className="form-label">
-          Source
-        </label>
-        <input
-          type="text"
-          name="source"
-          id="source"
-          className="form-control"
-          placeholder="Source"
-          aria-describedby="helpId"
-          defaultValue={defaults.source}
-        />
-        <small id="helpId" className="text-muted">
-          Source.
-        </small>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="content" className="form-label">
-          Content
-        </label>
-        <textarea
-          type="text"
-          name="content"
-          id="content"
-          className="form-control"
-          placeholder="Content"
-          rows="10"
-          defaultValue={defaults.content}
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+    <>
+      {responseData.id ? (
+        <div
+          className="alert alert-success mb-5 alert-dismissible fade show"
+          role="alert"
+        >
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+            onClick={handleAlertClose}
+          ></button>
+          {props.successMessage + " "}
+          <strong>
+            <Link to={"/news/" + responseData.id}>Go To Post</Link>
+          </strong>
+        </div>
+      ) : (
+        ""
+      )}
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="form-control"
+            placeholder="Title"
+            aria-describedby="helpId"
+            defaultValue={defaults.title ?? ""}
+          />
+          <small id="helpId" className="text-muted">
+            This is my First article and I say Hello.
+          </small>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="author" className="form-label">
+            Author
+          </label>
+          <input
+            type="text"
+            name="author"
+            id="author"
+            className="form-control"
+            placeholder="Author"
+            aria-describedby="helpId"
+            defaultValue={defaults.author ?? ""}
+          />
+          <small id="helpId" className="text-muted">
+            Ahmed Mahdy.
+          </small>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="source" className="form-label">
+            Source
+          </label>
+          <input
+            type="text"
+            name="source"
+            id="source"
+            className="form-control"
+            placeholder="Source"
+            aria-describedby="helpId"
+            defaultValue={defaults.source ?? ""}
+          />
+          <small id="helpId" className="text-muted">
+            Source.
+          </small>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="content" className="form-label">
+            Content
+          </label>
+          <textarea
+            type="text"
+            name="content"
+            id="content"
+            className="form-control"
+            placeholder="Content"
+            rows="10"
+            defaultValue={defaults.content ?? ""}
+          />
+        </div>
+        <button
+          type="submit"
+          disabled={isSubmitting}
+          className="btn btn-primary"
+        >
+          Submit
+        </button>
+      </form>
+    </>
   );
 }
 
